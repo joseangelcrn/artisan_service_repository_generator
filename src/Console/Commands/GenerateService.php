c00e691b0b686ddc148051b94c6a5292c4f9dd7c
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-12-19 20:58:42 +0100

    Check if repository params exists before Service creation, if doesnt script will create it before Service creation

--- src/Console/Commands/GenerateService.php
+++ src/Console/Commands/GenerateService.php
@@ -2,13 +2,15 @@
 
 namespace josanangel\ServiceRepositoryManager\Console\Commands;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class GenerateService extends GeneratorCommand
 {
     // El nombre del comando que ejecutarás en la consola
-    protected $signature = 'make:service {name}';
+    protected $signature = 'make:service {name} {--repositories=}';
 
     // La descripción del comando
     protected $description = 'Generate a service class';
@@ -24,14 +26,34 @@ class GenerateService extends GeneratorCommand
         $name = $this->normalizeClassName($name);
         $serviceClassName = $name."Service";
 
-        $shouldOverride = $this->shouldOverrideIfExists('service',[
-            'class_name' => $name,
-            'additional_message' => "Service already exists"
-        ]);
+        $repositories = $this->option('repositories');
+        $repositories = explode(',',$repositories);
+        $repositories = collect($repositories);
 
-        if (!$shouldOverride){
-            return false;
+        if ($repositories->isNotEmpty()){
+
+            $repositories = $repositories->map(function ($repository){
+                $path =  app_path("Repositories/$repository.php");
+                if (!File::exists($path)){
+                    $this->warn("Repository '$repository' doesnt exist, creating it");
+                    Artisan::call("make:repository" ,['name'=>$repository]);
+                }
+                return $path;
+            });
+
+
+
         }
+
+//todo remove comments when tests finish
+//        $shouldOverride = $this->shouldOverrideIfExists('service',[
+//            'class_name' => $name,
+//            'additional_message' => "Service already exists"
+//        ]);
+//
+//        if (!$shouldOverride){
+//            return false;
+//        }
 
         $this->generateFile('service',[
            'class_name'=>$name
