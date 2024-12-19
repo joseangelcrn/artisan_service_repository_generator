51d1ca3ac1496dac9dbd4f2b2666c96c782bcce6
josanangel <jose.angel.cabeza.rn@gmail.com>
2024-12-19 11:12:13 +0100

    First Commit

---
+++ src/ServiceRepositoryManagerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace josanangel\ServiceRepositoryManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceRepositoryManagerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Registra vistas, migraciones, rutas, etc.
+    }
+
+    public function register()
+    {
+        // Registra bindings en el contenedor de servicios
+    }
+}
