cbcd950f42f6c178436b7c73571418019ffe2206
josanangel <jose.angel.cabeza.rn@gmail.com>
2025-01-04 15:25:39 +0100

    Repository tests

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -12,4 +12,43 @@ class RepositoryTest extends TestCase
 
         $this->artisan("make:repository $repositoryName")->assertOk();
     }
+    public function test_command_creates_single_repository_with_modules_enabled_with_opt_module_and_namespace_keyword_must_success(): void
+    {
+        $repositoryName = $this->randomName();
+        $moduleName = $this->randomName();
+        $this->changeConfig('modules',true);
+        $this->changeConfig('repositories.namespace','App\{module_name}\Repositories');
+
+        $this->artisan("make:repository $repositoryName --module=$moduleName")->assertOk();
+    }
+    public function test_command_creates_single_repository_with_modules_enabled_without_opt_module_and_namespace_keyword_must_fail(): void
+    {
+        $repositoryName = $this->randomName();
+        $this->changeConfig('modules',true);
+        $this->changeConfig('repositories.namespace','App\Repositories');
+
+        try{
+            $this->artisan("make:repository $repositoryName");
+        }catch (\Exception $e){
+            $this->assertEquals(
+                $e->getMessage(),
+                'Namespace require a "{module_name}" keyword if module config is enabled.'
+            );
+        }
+    }
+    public function test_command_creates_single_repository_with_modules_enabled_without_opt_module_must_fail(): void
+    {
+        $repositoryName = $this->randomName();
+        $this->changeConfig('modules',true);
+        $this->changeConfig('repositories.namespace','App\{module_name}\Repositories');
+
+        try{
+            $this->artisan("make:repository $repositoryName");
+        }catch (\Exception $e){
+            $this->assertEquals(
+                $e->getMessage(),
+                'Module can not be null if module config is enabled.'
+            );
+        }
+    }
 }
